531aae5df173a7385f348d634980cb6f1a79b947
MarkIsNotTaken <mdemesillo@gmail.com>
2018-03-01 11:56:57 -1000

    Orbs now don't exceed screen capacity

--- scripts/map.js
+++ scripts/map.js
@@ -1,15 +1,27 @@
 xCoord = () => {
-  return Math.floor(Math.random() * window.innerWidth)
+  return Math.floor(Math.random() * $(".test").height() - 20)
 }
 
 yCoord = () => {
-  return Math.floor(Math.random() * window.innerHeight)
+  return Math.floor(Math.random() * $(".test").width() - 20)
+}
+
+let images = ["styles/lightningBallBad.gif", "styles/lightningBallGood.gif"];
+
+randomImg = () => {
+  return (Math.floor(Math.random() * images.length) + 0)
 }
 
 randomButton = () => {
   let randX = xCoord();
   let randY = yCoord();
-  $('#map').append($('<img src="styles/lightningBallBad.gif" style="position:absolute; top:' + randX + 'px; left:' + randY + 'px; width: 20px; height: 20px; border-radius: 30px;" >'));
+  let randNum = randomImg();
+  console.log( $("#container").availHeight)
+  $('#map').append($(`<img src="${images[randNum]}" style="position:absolute; top:` + randX + `px; left:` + randY + `px; width: 20px; height: 20px; border-radius: 30px;" >`));
 }
 
-setInterval(randomButton, 1000)
+startGame = () => {
+  setInterval(randomButton, 0000)
+}
+
+startGame();
