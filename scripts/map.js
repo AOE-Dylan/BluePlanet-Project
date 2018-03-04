09486237423290bc4e09cddd5c952ca39c984334
kylelingat <kylejlingat@gmail.com>
2018-03-03 21:22:03 -1000

    merged js files

--- scripts/map.js
+++ scripts/map.js
@@ -19,7 +19,63 @@ randomButton = () => {
   $('#map').append($(`<img class="bubble" src="${images[randNum]}" style="top:` + randX + `px; left:` + randY + `px;" >`));
 }
 
+var bubbleGenerate;
 startGame = () => {
-  setInterval(randomButton, 300)
+  var bubbleGenerate =  setInterval(randomButton, 2000);
 }
 
+startGame();
+
+
+var background = document.getElementById('background')
+var settings = document.getElementById('settingsButton');
+var settingsMenu = document.getElementById('settingsMenu');
+var information = document.getElementById('informationButton');
+var informationMenu = document.getElementById('informationMenu');
+
+settings.addEventListener("click", function() {
+    settingsMenu.style.display = "block";
+    background.style.filter = "blur(15px)";
+    clearInterval(bubbleGenerate);
+
+    if(informationMenu.style.display == "block"){
+      informationMenu.style.display = "none";
+    }
+
+    background.addEventListener('click', function() {
+        settingsMenu.style.display = "none";
+        background.style.filter = "blur(0px)";
+    });
+
+});
+
+
+$('.menuClose').click(function() {
+    background.style.filter = "blur(0px)";
+    settingsMenu.style.display = "none";
+});
+
+information.addEventListener("click", function() {
+    informationMenu.style.display = "block";
+    background.style.filter = "blur(15px)";
+
+    if(settingsMenu.style.display == "block"){
+      settingsMenu.style.display = "none";
+    }
+
+    background.addEventListener('click', function() {
+        informationMenu.style.display = "none";
+        background.style.filter = "blur(0px)";
+    });
+
+});
+
+$('.menuClose').click(function() {
+    background.style.filter = "blur(0px)";
+    informationMenu.style.display = "none";
+});
+
+$('#changeatt').click(function(){
+    $('#renewableProgress').css('height', $('#renewableProgress').height() + 5);
+    $('#nonRenewableProgress').css('height', $('#nonRenewableProgress').height() + 5);
+});
