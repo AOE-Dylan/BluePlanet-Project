ecd66f5e54c58a83a8b3aa27faf0aa5593e01679
kylelingat <kylejlingat@gmail.com>
2018-03-03 19:11:35 -1000

    added settings,information, and rounds

--- scripts/menus.js
+++ scripts/menus.js
@@ -1,10 +1,49 @@
-      document.getElementById('settings').addEventListener("click", function(){
-      document.getElementById('box').style.display = "block";
-      document.getElementById('background').style.filter = "blur(3px)";
+var click = 0;
+var background = document.getElementById('background')
+var settings = document.getElementById('settingsButton');
+var settingsMenu = document.getElementById('settingsMenu');
+var information = document.getElementById('informationButton');
+var informationMenu = document.getElementById('informationMenu');
+
+settings.addEventListener("click", function() {
+    settingsMenu.style.display = "block";
+    background.style.filter = "blur(15px)";
+
+    if(informationMenu.style.display == "block"){
+      informationMenu.style.display = "none";
+    }
+
+    background.addEventListener('click', function() {
+        settingsMenu.style.display = "none";
+        background.style.filter = "blur(0px)";
+    });
+
 });
 
 
-    $('#boxclose').click(function(){
-      document.getElementById('box').style.display = "none";
-      document.getElementById('background').style.filter = "blur(0px)";
+$('.menuClose').click(function() {
+    background.style.filter = "blur(0px)";
+    settingsMenu.style.display = "none";
+});
+
+information.addEventListener("click", function() {
+    informationMenu.style.display = "block";
+    background.style.filter = "blur(15px)";
+
+    if(settingsMenu.style.display == "block"){
+      settingsMenu.style.display = "none";
+    }
+
+    background.addEventListener('click', function() {
+        informationMenu.style.display = "none";
+        background.style.filter = "blur(0px)";
+    });
+
+});
+
+
+$('.menuClose').click(function() {
+    background.style.filter = "blur(0px)";
+    informationMenu.style.display = "none";
+    console.log('buttio')
 });
