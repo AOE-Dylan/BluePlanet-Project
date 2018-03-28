668a135ce8c56df9044a6b588c9cec3e44dc04f1
kylelingat <kylejlingat@gmail.com>
2018-03-28 12:33:05 -1000

    cleaned code

--- scripts/map.js
+++ scripts/map.js
@@ -1,40 +1,40 @@
 let xCoord = () => {
-  return Math.floor(Math.random() * $("#map").height() - 20)
+    return Math.floor(Math.random() * $("#map").height() - 20)
 }
 
 let yCoord = () => {
-  return Math.floor(Math.random() * $("#map").width() - 120)
+    return Math.floor(Math.random() * $("#map").width() - 120)
 }
 
 let images = ["styles/redbubble.jpg", "styles/greenbubble.jpg", "styles/greenbubble.jpg", "styles/greenbubble.jpg"];
 
 let randomImg = () => {
-  return (Math.floor(Math.random() * images.length) + 0)
+    return (Math.floor(Math.random() * images.length) + 0)
 };
 
 let addBarGood = () => {
-  $('#renewableProgress').css('height', $('#renewableProgress').height() + 5);
+    $('#renewableProgress').css('height', $('#renewableProgress').height() + 5);
 };
 
 let addBarBad = () => {
-  $('#renewableProgress').css('height', $('#renewableProgress').height() + 15)
-  $('#nonRenewableProgress').css('height', $('#nonRenewableProgress').height() + 15);
+    $('#renewableProgress').css('height', $('#renewableProgress').height() + 15)
+    $('#nonRenewableProgress').css('height', $('#nonRenewableProgress').height() + 15);
 };
 
 dictateBar = () => {
-  event.target.style.WebkitAnimation = null;
-  event.target.style.display = "none";
-  if (event.target.getAttribute("src") !== images[0]) {
-    addBarGood()
-    event.target.onclick = null;
-  } else {
-    addBarBad()
-    event.target.onclick = null;
-  }
+    event.target.style.WebkitAnimation = null;
+    event.target.style.display = "none";
+    if (event.target.getAttribute("src") !== images[0]) {
+        addBarGood()
+        event.target.onclick = null;
+    } else {
+        addBarBad()
+        event.target.onclick = null;
+    }
 }
 
 let timeNum = () => {
-  return (Math.floor(Math.random() * 8) + 4)
+    return (Math.floor(Math.random() * 8) + 4)
 }
 
 var timePassed = [];
@@ -42,20 +42,20 @@ var remaining = document.getElementById('map').children;
 
 
 let randomButton = () => {
-  let randX = xCoord();
-  let randY = yCoord();
-  let randNum = randomImg();
-  let randTime = timeNum();
-  $('#map').append($(`<img class="bubble" id="${remaining.length}" onclick="dictateBar()" src="${images[randNum]}" style="top:` + randX + `px; left:` + randY + `px; opacity: 1;" >`));
-  let currBubble = $(`#` + `${remaining.length - 1}`)[0];
-  // timePassed.push(parseInt(currBubble.id));
-  if(currBubble.src !== images[0]){
-    currBubble.style.WebkitAnimation = "fading " + (randTime) + "s linear";
-    currBubble.style.animationFillMode = "forwards";
-  } else {
-    currBubble.style.WebkitAnimation = "fading " + ((randTime) * 2) + "s linear";
-    currBubble.style.animationFillMode = "forwards";
-  }
+    let randX = xCoord();
+    let randY = yCoord();
+    let randNum = randomImg();
+    let randTime = timeNum();
+    $('#map').append($(`<img class="bubble" id="${remaining.length}" onclick="dictateBar()" src="${images[randNum]}" style="top:` + randX + `px; left:` + randY + `px; opacity: 1;" >`));
+    let currBubble = $(`#` + `${remaining.length - 1}`)[0];
+    // timePassed.push(parseInt(currBubble.id));
+    if (currBubble.src !== images[0]) {
+        currBubble.style.WebkitAnimation = "fading " + (randTime) + "s linear";
+        currBubble.style.animationFillMode = "forwards";
+    } else {
+        currBubble.style.WebkitAnimation = "fading " + ((randTime) * 2) + "s linear";
+        currBubble.style.animationFillMode = "forwards";
+    }
 };
 //   for (var i = 0; i < timePassed.length; i++){
 //     timePassed[i]++
@@ -94,123 +94,120 @@ let timerFail = document.getElementById('timerFail');
 zoomAnimation.addEventListener("animationend", AnimationListener, false);
 
 $("#startZoom").click(function() {
-  startZoom.style.display = "none";
-  $('#beforeStart').addClass('animated zoomOut');
-  $('#zoomAnimation').addClass('addZoom');
+    startZoom.style.display = "none";
+    $('#beforeStart').addClass('animated zoomOut');
+    $('#zoomAnimation').addClass('addZoom');
 });
 
-function AnimationListener(){
-  notMap.style.display = "block";
-  beforeStart.style.display = "none";
-  $('#beforeStart').removeClass('animated zoomOut');
+function AnimationListener() {
+    notMap.style.display = "block";
+    beforeStart.style.display = "none";
+    $('#beforeStart').removeClass('animated zoomOut');
 }
-
-restart.addEventListener("click", function() {
-  $('#startButton').addClass('animated infinite rubberBand');
-  timerFail.style.display = "none";
-  background.style.filter = "blur(0px)";
-  startButton.style.display = "block";
-  notMap.style.display = "block";
-  sec = 5;
-  document.getElementById('timer').innerHTML = sec;
-  let div = document.createElement("DIV");
-  div.id = "map";
-  let newMap = document.getElementById('background').appendChild(div);
-  remaining = document.getElementById('map').children;
-});
 
 startButton.addEventListener("click", function() {
     $('#startButton').removeClass('animated infinite rubberBand');
+
     bubbleGenerate = setInterval(randomButton, 1000);
-    console.log(bubbleGenerate, "bubbleGeneration started");
     timer = setInterval(countdown, 1000);
-    console.log(timer, "Timer countdown started")
-    console.log('Game Started');
+
     gameStart = 1;
     gameActive = 1;
-    console.log('gameActive is', gameActive)
+
     startButton.style.display = "none";
     pauseButton.style.display = "block";
     settingsMenu.style.display = "none";
     informationMenu.style.display = "none";
 
+    console.log('Game Started');
+    console.log('gameActive is', gameActive);
+    console.log(timer, "Timer countdown started");
+    console.log(bubbleGenerate, "bubbleGeneration started");
+});
 
+pauseButton.addEventListener("click", function() {
+    pause();
+    gamePaused.style.display = "block";
+});
 
-    pauseButton.addEventListener("click", function() {
-      pause();
-      gamePaused.style.display = "block";
-    });
+resumeButton.addEventListener("click", function() {
+    resume();
+    gamePaused.style.display = "none";
+});
 
-    resumeButton.addEventListener("click", function() {
-      resume();
-      gamePaused.style.display = "none";
-    });
+settings.addEventListener("click", function() {
+    pause();
+    gamePaused.style.display = "none";
+    settingsMenu.style.display = "block";
+    if (informationMenu.style.display == "block") {
+        informationMenu.style.display = "none";
+    }
+});
 
-    settings.addEventListener("click", function() {
-      pause();
-    });
+information.addEventListener("click", function() {
+    pause();
+    gamePaused.style.display = "none";
+    informationMenu.style.display = "block";
+    if (settingsMenu.style.display == "block") {
+        settingsMenu.style.display = "none";
+    }
+});
 
-    information.addEventListener("click", function() {
-      pause();
-      gamePaused.style.display = "none";
-    });
+let pause = () => {
+    if (gameActive == 1 & gameStart == 1) {
+        clearInterval(bubbleGenerate);
+        clearInterval(timer);
+        console.log('game paused')
+        gameActive = 0;
+        background.style.filter = "blur(60px)";
+        pauseButton.style.display = "none";
+        resumeButton.style.display = "block";
+    }
+};
 
-  let pause = () => {
-    if(gameActive == 1 & gameStart == 1){
-    clearInterval(bubbleGenerate);
-    clearInterval(timer);
-    console.log('game paused')
-    gameActive = 0;
-    background.style.filter = "blur(60px)";
-    pauseButton.style.display = "none";
-    resumeButton.style.display = "block";
-  }
-  };
+let resume = () => {
+    if (gameStart == 1 & gameActive == 0) {
+        bubbleGenerate = setInterval(randomButton, 1000);
+        gameActive = 1;
+        console.log('game resumed')
+        background.style.filter = "blur(0px)";
+        settingsMenu.style.display = "none";
+        informationMenu.style.display = "none";
+        resumeButton.style.display = "none";
+        pauseButton.style.display = "block";
+        timer = setInterval(countdown, 1000);
+    }
+};
 
-  let resume = () => {
-    if(gameStart == 1 & gameActive == 0){
-    bubbleGenerate = setInterval(randomButton, 1000);
-    gameActive = 1;
-    console.log('game resumed')
-    background.style.filter = "blur(0px)";
-    settingsMenu.style.display = "none";
-    informationMenu.style.display = "none";
-    resumeButton.style.display = "none";
-    pauseButton.style.display = "block";
-    timer = setInterval(countdown, 1000);
-  }
-  };
-
-    $('.menuClose').click(function() {
-      if(gameStart == 1 & gameActive == 0){
+$('.menuClose').click(function() {
+    if (gameStart == 1 & gameActive == 0) {
         bubbleGenerate = setInterval(randomButton, 1000);
         timer = setInterval(countdown, 1000);
         console.log('Menu Closed, Starting Generating');
         pauseButton.style.display = "block";
         resumeButton.style.display = "none";
+        background.style.filter = "blur(0px)";
+        informationMenu.style.display = "none";
+        settingsMenu.style.display = "none";
         gameActive = 1;
-      };
-    });
-});
-
-settings.addEventListener("click", function() {
-    settingsMenu.style.display = "block";
-
-    if(informationMenu.style.display == "block"){
+    } else {
+      background.style.filter = "blur(0px)";
       informationMenu.style.display = "none";
-    }
-});
-
-information.addEventListener("click", function() {
-    informationMenu.style.display = "block";
-
-    if(settingsMenu.style.display == "block"){
       settingsMenu.style.display = "none";
     }
 });
 
-$('.menuClose').click(function() {
+restart.addEventListener("click", function() {
+    timerFail.style.display = "none";
     background.style.filter = "blur(0px)";
-    informationMenu.style.display = "none";
-    settingsMenu.style.display = "none";
+    startButton.style.display = "block";
+    notMap.style.display = "block";
+
+    let div = document.createElement("DIV");
+    div.id = "map";
+    let newMap = document.getElementById('background').appendChild(div);
+    remaining = document.getElementById('map').children;
+    sec = 5;
+    document.getElementById('timer').innerHTML = sec;
+    $('#startButton').addClass('animated infinite rubberBand');
 });
